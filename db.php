c657487ceac03fc653ba2da59e2deaf6ce2becb9
Haritha1234591 <harithab2023@gmail.com>
2025-04-29 18:12:36 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-    $con = mysqli_connect("127.0.0.1","root","","bnb");
+    $con = mysqli_connect("bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","zqd50en3jpqa722r","w6wqlkivyrhihpm6","eanoyr6947rok9be");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
